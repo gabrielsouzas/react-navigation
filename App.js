b55ea6a152c01ee8d98cd2f33087d04e57275f9e
Gabriel Souza <104937852+gabrielsouzas@users.noreply.github.com>
2023-06-05 16:42:53 -0300

    Estrutura inicial

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Contacts from './src/pages/Contacts/';
+import Information from './src/pages/Information/';
+
+const Stack = createStackNavigator();
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+     <Stack.Navigator >{/* DICA */}
+       <Stack.Screen name="Contacts" component={Contacts}/>
+       <Stack.Screen name="Information" component={Information}/>
+     </Stack.Navigator>
+   </NavigationContainer>
   );
 }
 
